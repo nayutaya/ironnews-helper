558d4c76bb6c3697c5a587c9eae34c625dbdae5c
Yuya Kato <yuyakato@gmail.com>
2009-11-04 11:22:36 +0900

    はてなブックマークへの登録の検証コードを追加

--- app.py
+++ app.py
@@ -1,13 +1,56 @@
 # -*- coding: utf-8 -*-
 
-print "Content-Type: text/html"
+from base64 import b64encode
+from time import time
+from random import random
+from datetime import datetime
+import sha
+import httplib
+
+def create_wsse_auth(username, password):
+  created = datetime.now().isoformat() + "Z"
+  nonce   = b64encode(sha.sha(str(time() + random())).digest())
+  digest  = b64encode(sha.sha(nonce + created + password).digest())
+  wsse  = 'UsernameToken Username="%(u)s", PasswordDigest="%(p)s", Nonce="%(n)s", Created="%(c)s"'
+  value = dict(u = username, p = digest, n = nonce, c = created)
+  return wsse % value
+
+def read_credential():
+  f = open("config/hatena.id")
+  username = f.readline().strip()
+  password = f.readline().strip()
+  f.close()
+  return (username, password)
+
+
+print "Content-Type: text/plain"
 print ""
-print "hello"
 
-f = open("config/hatena.id")
-username = f.readline()
-password = f.readline()
-f.close()
+username, password = read_credential()
+useragent = "ironnews"
 
-print username
-print password
+url = "http://www.asahi.com/national/update/1102/SEB200911020008.html"
+
+
+wsse = create_wsse_auth(username, password)
+
+request  = '<entry xmlns="http://purl.org/atom/ns#">'
+request += '<title>title</title>'
+request += '<link rel="related" type="text/html" href="' + url + '" />'
+request += '</entry>'
+
+# Set header for HTTPConnection
+headers = {
+  "Content-Type": "text/xml",
+  "User-Agent"  : useragent,
+  "X-WSSE"      : wsse,
+}
+
+con = httplib.HTTPConnection("b.hatena.ne.jp")
+con.request("POST", "/atom/post", request, headers)
+
+res = con.getresponse()
+#print res
+
+response = dict(status=res.status, reason=res.reason, data=res.read())
+print response["data"]
