37db4137fe39fc7aaf11800d953285da6ff08d6e
Yuya Kato <yuyakato@gmail.com>
2009-11-04 14:37:28 +0900

    キャッシュ処理を追加

--- app.py
+++ app.py
@@ -4,36 +4,55 @@ import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
 from BeautifulSoup import BeautifulSoup
 import json
+import logging
 
 from hatenabookmark import HatenaBookmark
 
+import sha
 class TitleFetcher:
   def __init__(self):
     username, password = HatenaBookmark.read_credential()
     self.hatena_bm = HatenaBookmark(username, password)
 
   def fetch_title(self, url):
-    response = self.hatena_bm.post(url)
-    xml      = response.read()
+    logging.info("get title: " + url)
+    key = self.create_key(url)
+    logging.info("key: " + key)
 
-    doc   = BeautifulSoup(xml)
-    title = doc.find("title")
+    cache = memcache.get(key)
+    if cache is not None:
+      logging.info("cache hit")
+      return cache
+    else:
+      logging.info("cache miss")
+      response = self.hatena_bm.post(url)
+      xml      = response.read()
+      doc      = BeautifulSoup(xml)
+      title    = doc.find("title").string.strip()
+      memcache.add(key, title, 60 * 60)
+      return title
 
-    return title.string.strip()
+  def create_key(self, url):
+    return "hatena_bookmark_title_" + sha.sha(url).hexdigest()
 
 class HatenaBookmarkGetTitleApi(webapp.RequestHandler):
   def get(self):
     fetcher = TitleFetcher()
-    #self.response.out.write(html)
 
     result = {}
-    urls = [self.request.get("url%02i" % (i + 1)) for i in range(10)]
-    for url in urls:
+    for i in range(10):
+      number = i + 1
+      url    = self.request.get("url%i" % number)
+
       if url != "":
         title = fetcher.fetch_title(url)
-        result[url] = title
+        result[number] = {
+          "url": url,
+          "title": title,
+        }
 
     self.response.out.write(json.write(result))
 
