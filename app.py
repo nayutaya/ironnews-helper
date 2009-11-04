62553fdabc22e042afa802512c2f0ad5beb9d8fd
Yuya Kato <yuyakato@gmail.com>
2009-11-04 11:01:42 +0900

    はてなの認証情報を読み込む検証コードを追加

--- app.py
+++ app.py
@@ -3,3 +3,11 @@
 print "Content-Type: text/html"
 print ""
 print "hello"
+
+f = open("config/hatena.id")
+username = f.readline()
+password = f.readline()
+f.close()
+
+print username
+print password
