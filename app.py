4c9d9400253f85715f68b2bf6c295a3cfd2146d7
Yuya Kato <yuyakato@gmail.com>
2009-11-04 11:52:04 +0900

    リファクタリング

--- app.py
+++ app.py
@@ -7,62 +7,72 @@ from datetime import datetime
 import sha
 import httplib
 
-def create_created():
-  return datetime.now().isoformat() + "Z"
+class WSSE:
+  @classmethod
+  def create_created(cls):
+    return datetime.now().isoformat() + "Z"
 
-def create_nonce():
-  return b64encode(sha.sha(str(time() + random())).digest())
+  @classmethod
+  def create_nonce(cls):
+    return b64encode(sha.sha(str(time() + random())).digest())
 
-def create_digest(passowrd, nonce, created):
-  return b64encode(sha.sha(nonce + created + password).digest())
+  @classmethod
+  def create_digest(cls, passowrd, nonce, created):
+    return b64encode(sha.sha(nonce + created + password).digest())
 
-def format_token(username, digest, nonce, created):
-  format = 'UsernameToken Username="%(u)s", PasswordDigest="%(p)s", Nonce="%(n)s", Created="%(c)s"'
-  value  = dict(u = username, p = digest, n = nonce, c = created)
-  return format % value
+  @classmethod
+  def format_token(cls, username, digest, nonce, created):
+    format = 'UsernameToken Username="%(u)s", PasswordDigest="%(p)s", Nonce="%(n)s", Created="%(c)s"'
+    value  = dict(u = username, p = digest, n = nonce, c = created)
+    return format % value
 
-def create_wsse_auth(username, password):
-  created = create_created()
-  nonce   = create_nonce()
-  digest  = create_digest(password, nonce, created)
-  return format_token(username, digest, nonce, created)
+  @classmethod
+  def create_token(cls, username, password):
+    created = cls.create_created()
+    nonce   = cls.create_nonce()
+    digest  = cls.create_digest(password, nonce, created)
+    return cls.format_token(username, digest, nonce, created)
 
-def read_credential():
-  f = open("config/hatena.id")
-  username = f.readline().strip()
-  password = f.readline().strip()
-  f.close()
-  return (username, password)
+class HatenaBookmark:
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
 
-def create_request_xml(url):
-  return (
-    '<entry xmlns="http://purl.org/atom/ns#">'
-    '<title>title</title>'
-    '<link rel="related" type="text/html" href="') + url + ('" />'
-    '</entry>')
+  @classmethod
+  def read_credential(cls):
+    f = open("config/hatena.id")
+    username = f.readline().strip()
+    password = f.readline().strip()
+    f.close()
+    return (username, password)
 
+  def create_http_header(self):
+    return {
+      "Content-Type": "text/xml",
+      "User-Agent"  : "ironnews",
+      "X-WSSE"      : WSSE.create_token(self.username, self.password),
+    }
+
+  def create_post_request_xml(self, url):
+    return (
+      '<entry xmlns="http://purl.org/atom/ns#">'
+      '<title>title</title>'
+      '<link rel="related" type="text/html" href="') + url + ('" />'
+      '</entry>')
+
+  def post(self, url):
+    header  = hb.create_http_header()
+    request = hb.create_post_request_xml(url)
+    connection = httplib.HTTPConnection("b.hatena.ne.jp")
+    connection.request("POST", "/atom/post", request, header)
+    return connection.getresponse()
 
 print "Content-Type: text/plain"
 print ""
 
-username, password = read_credential()
-useragent = "ironnews"
+username, password = HatenaBookmark.read_credential()
+hb = HatenaBookmark(username, password)
 
-wsse = create_wsse_auth(username, password)
-
-url = "http://www.asahi.com/national/update/1102/SEB200911020008.html"
-request = create_request_xml(url)
-
-# Set header for HTTPConnection
-headers = {
-  "Content-Type": "text/xml",
-  "User-Agent"  : useragent,
-  "X-WSSE"      : wsse,
-}
-
-connection = httplib.HTTPConnection("b.hatena.ne.jp")
-connection.request("POST", "/atom/post", request, headers)
-
-response = connection.getresponse()
+response = hb.post("http://www.asahi.com/national/update/1031/OSK200910310079.html")
 
 print response.read()
