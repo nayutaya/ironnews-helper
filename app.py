2f6642993d8bb3cb9e8e4db8a8bc17c5cac4fd69
Yuya Kato <yuyakato@gmail.com>
2009-11-06 11:51:43 +0900

    ファイル名を変更

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ sys.path.append("lib")
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-import hatenabookmark_handler
+import hatena_bookmark_handler
 
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
     [
-      (r"/hatena-bookmark/get-title",   hatenabookmark_handler.GetTitleApi),
-      (r"/hatena-bookmark/get-summary", hatenabookmark_handler.GetSummaryApi),
+      (r"/hatena-bookmark/get-title",   hatena_bookmark_handler.GetTitleApi),
+      (r"/hatena-bookmark/get-summary", hatena_bookmark_handler.GetSummaryApi),
     ],
     debug = True)
   run_wsgi_app(application)
