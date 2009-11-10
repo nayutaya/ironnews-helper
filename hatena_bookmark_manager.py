d64a619eb450308127395d3e3364d691e6c9e4c0
Yuya Kato <yuyakato@gmail.com>
2009-11-11 01:00:33 +0900

    ログの出力処理を削除

--- hatena_bookmark_manager.py
+++ hatena_bookmark_manager.py
@@ -20,36 +20,28 @@ class HatenaBookmarkManager:
     finally:
       f.close()
 
+  def create_title_key(self, url):
+    return "hatena_bookmark_title_" + sha.sha(url).hexdigest()
+
   def get_title(self, url):
-    logging.info("get title: " + url)
     key   = self.create_title_key(url)
     value = memcache.get(key)
 
     if value is None:
-      logging.info("cache miss")
       value = HatenaBookmark.get_title(url, self.username, self.password)
       memcache.add(key, value, self.ttl)
-    else:
-      logging.info("cache hit")
-
-    return value
-
-  def create_title_key(self, url):
-    return "hatena_bookmark_title_" + sha.sha(url).hexdigest()
-
-  def get_summary(self, url):
-    logging.info("get summary " + url)
-    key = self.create_summary_key(url)
-    value = memcache.get(key)
-
-    if value is None:
-      logging.info("cache miss")
-      value = HatenaBookmark.get_summary(url)
-      memcache.add(key, value, self.ttl)
-    else:
-      logging.info("cache hit")
 
     return value
 
   def create_summary_key(self, url):
     return "hatena_bookmark_summary_" + sha.sha(url).hexdigest()
+
+  def get_summary(self, url):
+    key   = self.create_summary_key(url)
+    value = memcache.get(key)
+
+    if value is None:
+      value = HatenaBookmark.get_summary(url)
+      memcache.add(key, value, self.ttl)
+
+    return value
