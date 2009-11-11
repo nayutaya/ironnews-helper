ea399e6bb3cbbbc2f6182aa73ff4ebfc7f247eeb
Yuya Kato <yuyakato@gmail.com>
2009-11-11 13:32:11 +0900

    検索キーワード、検索数をパラメータで指定できるように変更

--- google_news_handler.py
+++ google_news_handler.py
@@ -8,13 +8,12 @@ from google_news_manager import GoogleNewsManager
 
 class SearchApi(webapp.RequestHandler):
   def get(self):
+    keyword  = self.request.get("keyword")
+    num      = self.request.get("num", default_value = "10")
     callback = self.request.get("callback")
 
-    keyword = u"私鉄"
-    num     = 10
-
     news_manager = GoogleNewsManager()
-    result = news_manager.search(keyword, num)
+    result = news_manager.search(keyword, int(num))
 
     json = simplejson.dumps(result, separators=(',',':'))
     if callback != "": json = callback + "(" + json + ")"
