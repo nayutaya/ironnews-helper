b551980d5fe0504eebf057c271bb2746b1d3e2b0
Yuya Kato <yuyakato@gmail.com>
2009-11-11 14:29:45 +0900

    キャッシュ情報の表示内容を整形

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append("lib")
 
 import os
+import re
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -17,15 +18,18 @@ class HomePage(webapp.RequestHandler):
   def get(self):
     stats  = memcache.get_stats()
     values = {
-      "hits"  : stats["hits"],
-      "misses": stats["misses"],
-      "items" : stats["items"],
-      "bytes" : stats["bytes"],
-      "ratio" : str(int(stats["hits"]) * 100 / (int(stats["hits"]) + int(stats["misses"]))),
+      "ratio" : (stats["hits"] * 100 / (stats["hits"] + stats["misses"])),
+      "hits"  : self.format_integer(stats["hits"]),
+      "misses": self.format_integer(stats["misses"]),
+      "items" : self.format_integer(stats["items"]),
+      "bytes" : self.format_integer(stats["bytes"]),
     }
     path = os.path.join(os.path.dirname(__file__), "home.html")
     html = template.render(path, values)
     self.response.out.write(html)
+
+  def format_integer(self, value):
+    return re.sub(r"(\d)(?=(\d{3})+(?!\d))", r"\1,", str(value))
 
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
