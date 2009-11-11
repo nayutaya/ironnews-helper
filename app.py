31fe79af58f92f9d559093a0f23019705d0cafa8
Yuya Kato <yuyakato@gmail.com>
2009-11-11 14:20:21 +0900

    トップページにキャッシュ状況の表示を追加

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 import sys
 sys.path.append("lib")
 
+import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
 
 import hatena_bookmark_handler
 import yahoo_keyphrase_handler
@@ -12,7 +15,17 @@ import google_news_handler
 
 class HomePage(webapp.RequestHandler):
   def get(self):
-    self.response.out.write("hello")
+    stats  = memcache.get_stats()
+    values = {
+      "hits"  : stats["hits"],
+      "misses": stats["misses"],
+      "items" : stats["items"],
+      "bytes" : stats["bytes"],
+      "ratio" : str(int(stats["hits"]) * 100 / (int(stats["hits"]) + int(stats["misses"]))),
+    }
+    path = os.path.join(os.path.dirname(__file__), "home.html")
+    html = template.render(path, values)
+    self.response.out.write(html)
 
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
