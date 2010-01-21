6b66314edb8b29264e683c599a1f7fbad57c9fc6
Yuya Kato <yuyakato@gmail.com>
2010-01-21 17:15:44 +0900

    はてなブックマークから都道府県名を取得する処理を追加

--- ironnews-helper2/app.rb
+++ ironnews-helper2/app.rb
@@ -12,12 +12,5 @@ end
 require "hpricot"
 
 get "/hatena_bookmark/get_area" do
-  #url = "http://b.hatena.ne.jp/entry/gigazine.net/index.php?/news/comments/20100121_paragon_backup_recovery_free/"
-  url = "http://b.hatena.ne.jp/entry/alfalfalfa.com/archives/383013.html"
-
-  res = AppEngine::URLFetch.fetch(url)
-  src = res.body
-  doc = Hpricot.parse(src)
-
-  return doc.inspect
+  erb(:test)
 end
