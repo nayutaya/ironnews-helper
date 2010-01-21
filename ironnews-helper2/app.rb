26bafb6934ed3dd60983935a102eb39c4de09b7c
Yuya Kato <yuyakato@gmail.com>
2010-01-21 16:55:09 +0900

    URLフェッチの実験コードを追加

--- ironnews-helper2/app.rb
+++ ironnews-helper2/app.rb
@@ -1,5 +1,18 @@
 #! ruby -Ku
 
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 get "/" do
   "ironnews-helper2 v4"
+end
+
+
+get "/hatena_bookmark/get_area" do
+  url = "http://b.hatena.ne.jp/entry/gigazine.net/index.php?/news/comments/20100121_paragon_backup_recovery_free/"
+
+  res = AppEngine::URLFetch.fetch(url)
+  return res.body
 end
