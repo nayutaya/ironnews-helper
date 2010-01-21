5df61abb0f7f49c1e2056cf5a5811c5cc660661b
Yuya Kato <yuyakato@gmail.com>
2010-01-21 16:59:29 +0900

    hpricotを使う実験コードを追加

--- ironnews-helper2/app.rb
+++ ironnews-helper2/app.rb
@@ -9,10 +9,15 @@ get "/" do
   "ironnews-helper2 v4"
 end
 
+require "hpricot"
 
 get "/hatena_bookmark/get_area" do
-  url = "http://b.hatena.ne.jp/entry/gigazine.net/index.php?/news/comments/20100121_paragon_backup_recovery_free/"
+  #url = "http://b.hatena.ne.jp/entry/gigazine.net/index.php?/news/comments/20100121_paragon_backup_recovery_free/"
+  url = "http://b.hatena.ne.jp/entry/alfalfalfa.com/archives/383013.html"
 
   res = AppEngine::URLFetch.fetch(url)
-  return res.body
+  src = res.body
+  doc = Hpricot.parse(src)
+
+  return doc.inspect
 end
