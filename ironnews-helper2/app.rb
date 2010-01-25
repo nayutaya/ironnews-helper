739e145a6a764511a069ad1f1c793f5a9976839e
Yuya Kato <yuyakato@gmail.com>
2010-01-25 18:28:08 +0900

    GoogleNewsで検索を行うテストコードを追加

--- ironnews-helper2/app.rb
+++ ironnews-helper2/app.rb
@@ -12,9 +12,11 @@ get "/" do
   "ironnews-helper2 v4"
 end
 
+=begin
 get "/test" do
   erb(:test)
 end
+=end
 
 =begin
 get "/test" do
@@ -22,6 +24,13 @@ get "/test" do
   url = GoogleAjaxFeedsApi.create_url(:url => "http://api.tetsudo.com/news/atom.xml")
   response = AppEngine::URLFetch.fetch(url, :deadline => 10)
   return response.body
+end
+=end
+
+=begin
+get "/test" do
+  require "lib/google_news"
+  return GoogleNews.fetch_rss(:keyword => "鉄道")
 end
 =end
 
